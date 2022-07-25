3ba1cbd82641940b814222a2f3e7883f409b6cc4
bladewind21 <bladewind21@gmail.com>
2022-07-25 13:19:58 -0500

    initial commit

---
+++ src/main/java/com/bignerdranch/BignerdranchApplication.java
@@ -0,0 +1,13 @@
+package com.bignerdranch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BignerdranchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BignerdranchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bignerdranch/BignerdranchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bignerdranch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BignerdranchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
